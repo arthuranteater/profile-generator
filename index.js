8d322cade63bd74cb71e6d721917e4d55418715e
Hunt Applegate <huntapplegate@gmail.com>
2019-11-13 21:36:17 -0700

    a

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const fs = require("fs")
 const util = require("util")
 const axios = require('axios')
 const pdf = require('pdfkit')
-var htmlPDF = require('html-pdf');
+var htmlPDF = require('html-pdf')
+const dotenv = require('dotenv')
+const open = require('open')
+const path = require('path')
+
+// function writeToFile(fileName, data) {
+//   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+// }
+
+const opener = async (file, app) => {
+  await open(file, { app: app })
+}
 
 
 const colors = {
@@ -33,7 +44,8 @@ const colors = {
   }
 };
 
-const createPDF = (html) => {
+const createPDF = html => {
+  console.log(html)
   htmlPDF.create(html).toFile('./profile.pdf', function (err, res) {
     if (err) {
       console.log(err)
@@ -43,7 +55,38 @@ const createPDF = (html) => {
 }
 
 
-const writeFileAsync = util.promisify(fs.writeFile);
+
+const getUser = username => {
+  return axios
+    .get(
+      `https://api.github.com/users/${username}?client_id=${
+      process.env.CLIENT_ID
+      }&client_secret=${process.env.CLIENT_SECRET}`
+    )
+    .catch(err => {
+      console.log(`User not found`);
+      process.exit(1);
+    });
+}
+
+const getTotalStars = username => {
+  return axios
+    .get(
+      `https://api.github.com/users/${username}/repos?client_id=${
+      process.env.CLIENT_ID
+      }&client_secret=${process.env.CLIENT_SECRET}&per_page=100`
+    )
+    .then(response => {
+      // After getting user, count all their repository stars
+      return response.data.reduce((acc, curr) => {
+        acc += curr.stargazers_count;
+        return acc;
+      }, 0);
+    });
+}
+
+
+// const writeFileAsync = util.promisify(fs.writeFile);
 
 function promptUser() {
   return inquirer.prompt([
@@ -52,177 +95,182 @@ function promptUser() {
       name: "github",
       message: "Enter your GitHub Username"
     },
+    {
+      type: "list",
+      name: "color",
+      message: "What is your favorite color?",
+      choices: ["red", "blue", "green", "pink"]
+    }
   ]);
 }
 
-const getData = ({ github }) => {
-  const api = `https://api.github.com/users/${github}`
-  axios.get(api)
-}
+// const getData = ({ github }) => {
+//   const api = `https://api.github.com/users/${github}`
+//   axios.get(api)
+// }
 
-const buildPdf = () => {
-  const doc = new PDF();
-  doc.pipe(fs.createWriteStream('output.pdf'))
-}
+// const buildPdf = () => {
+//   const doc = new PDF();
+//   doc.pipe(fs.createWriteStream('output.pdf'))
+// }
 
-function generateHTML(answers) {
-  return `
-    <!DOCTYPE html>
-    <html lang="en">
-       <head>
-          <meta charset="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
-          <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
-          <title>Document</title>
-          <style>
-              @page {
-                margin: 0;
-              }
-             *,
-             *::after,
-             *::before {
-             box-sizing: border-box;
-             }
-             html, body {
-             padding: 0;
-             margin: 0;
-             }
-             html, body, .wrapper {
-             height: 100%;
-             }
-             .wrapper {
-             background-color: ${colors[data.color].wrapperBackground};
-             padding-top: 100px;
-             }
-             body {
-             background-color: white;
-             -webkit-print-color-adjust: exact !important;
-             font-family: 'Cabin', sans-serif;
-             }
-             main {
-             background-color: #E9EDEE;
-             height: auto;
-             padding-top: 30px;
-             }
-             h1, h2, h3, h4, h5, h6 {
-             font-family: 'BioRhyme', serif;
-             margin: 0;
-             }
-             h1 {
-             font-size: 3em;
-             }
-             h2 {
-             font-size: 2.5em;
-             }
-             h3 {
-             font-size: 2em;
-             }
-             h4 {
-             font-size: 1.5em;
-             }
-             h5 {
-             font-size: 1.3em;
-             }
-             h6 {
-             font-size: 1.2em;
-             }
-             .photo-header {
-             position: relative;
-             margin: 0 auto;
-             margin-bottom: -50px;
-             display: flex;
-             justify-content: center;
-             flex-wrap: wrap;
-             background-color: ${colors[data.color].headerBackground};
-             color: ${colors[data.color].headerColor};
-             padding: 10px;
-             width: 95%;
-             border-radius: 6px;
-             }
-             .photo-header img {
-             width: 250px;
-             height: 250px;
-             border-radius: 50%;
-             object-fit: cover;
-             margin-top: -75px;
-             border: 6px solid ${colors[data.color].photoBorderColor};
-             box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
-             }
-             .photo-header h1, .photo-header h2 {
-             width: 100%;
-             text-align: center;
-             }
-             .photo-header h1 {
-             margin-top: 10px;
-             }
-             .links-nav {
-             width: 100%;
-             text-align: center;
-             padding: 20px 0;
-             font-size: 1.1em;
-             }
-             .nav-link {
-             display: inline-block;
-             margin: 5px 10px;
-             }
-             .workExp-date {
-             font-style: italic;
-             font-size: .7em;
-             text-align: right;
-             margin-top: 10px;
-             }
-             .container {
-             padding: 50px;
-             padding-left: 100px;
-             padding-right: 100px;
-             }
-    
-             .row {
-               display: flex;
-               flex-wrap: wrap;
-               justify-content: space-between;
-               margin-top: 20px;
-               margin-bottom: 20px;
-             }
-    
-             .card {
-               padding: 20px;
-               border-radius: 6px;
-               background-color: ${colors[data.color].headerBackground};
-               color: ${colors[data.color].headerColor};
-               margin: 20px;
-             }
-             
-             .col {
-             flex: 1;
-             text-align: center;
-             }
-    
-             a, a:hover {
-             text-decoration: none;
-             color: inherit;
-             font-weight: bold;
-             }
-    
-             @media print { 
-              body { 
-                zoom: .75; 
-              } 
-             }
-          </style>
-       </head>
-       <body>
-          <div class="wrapper">
-             <div class="photo-header">
-                <img src="${data.avatar_url}" alt="Photo of ${data.name}" />
-                <h1>Hi!</h1>
-                <h2>
-                My name is ${data.name}!</h1>
-                <h5>${data.company ? `Currently @ ${data.company}` : ""}</h5>
-                <nav class="links-nav">
-                   ${
+function generateHTML(data) {
+  return `<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"/>
+      <link href="https://fonts.googleapis.com/css?family=BioRhyme|Cabin&display=swap" rel="stylesheet">
+      <title>Document</title>
+      <style>
+          @page {
+            margin: 0;
+          }
+         *,
+         *::after,
+         *::before {
+         box-sizing: border-box;
+         }
+         html, body {
+         padding: 0;
+         margin: 0;
+         }
+         html, body, .wrapper {
+         height: 100%;
+         }
+         .wrapper {
+         background-color: ${colors[data.color].wrapperBackground};
+         padding-top: 100px;
+         }
+         body {
+         background-color: white;
+         -webkit-print-color-adjust: exact !important;
+         font-family: 'Cabin', sans-serif;
+         }
+         main {
+         background-color: #E9EDEE;
+         height: auto;
+         padding-top: 30px;
+         }
+         h1, h2, h3, h4, h5, h6 {
+         font-family: 'BioRhyme', serif;
+         margin: 0;
+         }
+         h1 {
+         font-size: 3em;
+         }
+         h2 {
+         font-size: 2.5em;
+         }
+         h3 {
+         font-size: 2em;
+         }
+         h4 {
+         font-size: 1.5em;
+         }
+         h5 {
+         font-size: 1.3em;
+         }
+         h6 {
+         font-size: 1.2em;
+         }
+         .photo-header {
+         position: relative;
+         margin: 0 auto;
+         margin-bottom: -50px;
+         display: flex;
+         justify-content: center;
+         flex-wrap: wrap;
+         background-color: ${colors[data.color].headerBackground};
+         color: ${colors[data.color].headerColor};
+         padding: 10px;
+         width: 95%;
+         border-radius: 6px;
+         }
+         .photo-header img {
+         width: 250px;
+         height: 250px;
+         border-radius: 50%;
+         object-fit: cover;
+         margin-top: -75px;
+         border: 6px solid ${colors[data.color].photoBorderColor};
+         box-shadow: rgba(0, 0, 0, 0.3) 4px 1px 20px 4px;
+         }
+         .photo-header h1, .photo-header h2 {
+         width: 100%;
+         text-align: center;
+         }
+         .photo-header h1 {
+         margin-top: 10px;
+         }
+         .links-nav {
+         width: 100%;
+         text-align: center;
+         padding: 20px 0;
+         font-size: 1.1em;
+         }
+         .nav-link {
+         display: inline-block;
+         margin: 5px 10px;
+         }
+         .workExp-date {
+         font-style: italic;
+         font-size: .7em;
+         text-align: right;
+         margin-top: 10px;
+         }
+         .container {
+         padding: 50px;
+         padding-left: 100px;
+         padding-right: 100px;
+         }
+
+         .row {
+           display: flex;
+           flex-wrap: wrap;
+           justify-content: space-between;
+           margin-top: 20px;
+           margin-bottom: 20px;
+         }
+
+         .card {
+           padding: 20px;
+           border-radius: 6px;
+           background-color: ${colors[data.color].headerBackground};
+           color: ${colors[data.color].headerColor};
+           margin: 20px;
+         }
+         
+         .col {
+         flex: 1;
+         text-align: center;
+         }
+
+         a, a:hover {
+         text-decoration: none;
+         color: inherit;
+         font-weight: bold;
+         }
+
+         @media print { 
+          body { 
+            zoom: .75; 
+          } 
+         }
+      </style>
+   </head>
+   <body>
+      <div class="wrapper">
+         <div class="photo-header">
+            <img src="${data.avatar_url}" alt="Photo of ${data.name}" />
+            <h1>Hi!</h1>
+            <h2>
+            My name is ${data.name}!</h1>
+            <h5>${data.company ? `Currently @ ${data.company}` : ""}</h5>
+            <nav class="links-nav">
+               ${
     data.location
       ? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="https://www.google.com/maps/place/${
       data.location
@@ -231,71 +279,78 @@ function generateHTML(answers) {
       }</a>`
       : ""
     }
-                   <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
+               <a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
     data.html_url
     }"><i class="fab fa-github-alt"></i> GitHub</a>
-                   ${
+               ${
     data.blog
       ? `<a class="nav-link" target="_blank" rel="noopener noreferrer" href="${
       data.blog
       }"><i class="fas fa-rss"></i> Blog</a>`
       : ""
     }
-                </nav>
-             </div>
-             <main>
-                <div class="container">
-                <div class="row">
-                   <div class="col">
-                      <h3>${data.bio ? `${data.bio}` : ""}</h3>
-                   </div>
-                   </div>
-                   <div class="row">
-                   <div class="col">
-                      <div class="card">
-                        <h3>Public Repositories</h3>
-                        <h4>${data.public_repos}</h4>
-                      </div>
-                   </div>
-                    <div class="col">
-                    <div class="card">
-                      <h3>Followers</h3>
-                      <h4>${data.followers}</h4>
-                    </div>
-                   </div>
-                   </div>
-                   <div class="row">
-                   <div class="col">
-                   <div class="card">
-                      <h3>GitHub Stars</h3>
-                      <h4>${data.stars}</h4>
-                      </div>
-                   </div>
-                    <div class="col">
-                    <div class="card">
-                      <h3>Following</h3>
-                      <h4>${data.following}</h4>
-                      </div>
-                   </div>
-                   </div>
+            </nav>
+         </div>
+         <main>
+            <div class="container">
+            <div class="row">
+               <div class="col">
+                  <h3>${data.bio ? `${data.bio}` : ""}</h3>
+               </div>
+               </div>
+               <div class="row">
+               <div class="col">
+                  <div class="card">
+                    <h3>Public Repositories</h3>
+                    <h4>${data.public_repos}</h4>
+                  </div>
+               </div>
+                <div class="col">
+                <div class="card">
+                  <h3>Followers</h3>
+                  <h4>${data.followers}</h4>
                 </div>
-             </main>
-          </div>
-       </body>
-    </html>`
+               </div>
+               </div>
+               <div class="row">
+               <div class="col">
+               <div class="card">
+                  <h3>GitHub Stars</h3>
+                  <h4>${data.stars}</h4>
+                  </div>
+               </div>
+                <div class="col">
+                <div class="card">
+                  <h3>Following</h3>
+                  <h4>${data.following}</h4>
+                  </div>
+               </div>
+               </div>
+            </div>
+         </main>
+      </div>
+   </body>
+</html>`;
 }
+
+
 promptUser()
-  .then(function (answers) {
-    const html = generateHTML(answers).then((html) => {
-
+  .then(({ color, github }) => {
+    console.log('getting github data')
+    getUser(github).then(response => getTotalStars(github).then(stars => {
+      console.log('response.data', response.data)
+      return generateHTML({ stars, color, ...response.data })
     })
-
-    return {}
+    )
+      .then(html => {
+        console.log('creating pdf')
+        console.log(html)
+        createPDF(html)
+      }).then(() => {
+        console.log("opening pdf");
+        opener(path.join(process.cwd(), "profile.pdf"), 'google chrome')
+      })
+      .catch(function (err) {
+        console.log(err);
+      })
   })
-  .then(function () {
-
-    console.log("Successfully wrote to index.html");
-  })
-  .catch(function (err) {
-    console.log(err);
-  });
